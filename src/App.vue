0c4e14fd6e52b7a89947e39a90f7b31d4d3f2c67
Chiu Tai Lin <ctl800303@gmail.com>
2020-03-30 17:48:03 +0800

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,16 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
-
   <div>
     <full-page :options="options">
-      <div class="section">
+      <!-- <div class="section">
         <cover />
-      </div>
+      </div> -->
       <div class="section">
         <div id="wrapper">
           <graph />
         </div>
       </div>
-      <div class="section" >
+      <!-- <div class="section" >
         <div id="outer" style="width:1200px;">
           <div id="divl">
             <chart />
@@ -52,7 +35,7 @@
             <chartClosenessCentrality />
           </div>
         </div>
-      </div>
+      </div> -->
     </full-page>
   </div>
 
@@ -61,57 +44,25 @@
 </template>
 
 <script>
-import cover from "./components/Cover.vue";
+// import cover from "./components/Cover.vue";
 import graph from "./components/Graph.vue";
-import chart from "./components/Chart.vue";
-import chartDegreeCentrality from "./components/ChartDegreeCentrality.vue";
-import chartClosenessCentrality from "./components/ChartClosenessCentrality.vue";
+// import chart from "./components/Chart.vue";
+// import chartDegreeCentrality from "./components/ChartDegreeCentrality.vue";
+// import chartClosenessCentrality from "./components/ChartClosenessCentrality.vue";
 
 export default {
   name: 'app',
   components: {
-    cover,
-    graph,
-    chart,
-    chartDegreeCentrality,
-    chartClosenessCentrality
+    // cover,
+    graph
+    // chart,
+    // chartDegreeCentrality,
+    // chartClosenessCentrality
   },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
 #wrapper {
   margin: auto;
 }
