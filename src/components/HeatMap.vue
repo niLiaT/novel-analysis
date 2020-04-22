a04462c5ed8bbd195bc3b9696cb5a68106f07b27
Chiu Tai Lin <ctl800303@gmail.com>
2020-04-23 02:16:21 +0800

    Data generating is completed

--- src/components/HeatMap.vue
+++ src/components/HeatMap.vue
@@ -29,72 +29,72 @@ export default {
       },
       series: [{
         name: 'Sunday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('sun')
         },
         {
           name: 'Saturday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('sat')
         },
         {
           name: 'Friday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('fri')
         },
         {
           name: 'Thursday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('thu')
         },
         {
           name: 'Wednesday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('wed')
         },
         {
           name: 'Tuesday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('tue')
         },
         {
           name: 'Monday',
-          data: this.generateData(24, {
-            min: 0,
-            max: 90
-          })
-          // data: this.generateData()
+          // data: this.generateData(24, {
+          //   min: 0,
+          //   max: 90
+          // })
+          data: this.generateData('mon')
         }
       ],
     }
   },
 
   methods: {
-    generateData(count, yrange) {
+    generateData(day) {
       var i = 0;
       var series = [];
-      while (i < count) {
+      while (i < 24) {
         var x = (i).toString();
         // var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-        var y = box[this.userId].mon[i]
+        var y = box[this.userId][day][i]
         series.push({
           x: x,
           y: y
@@ -110,8 +110,8 @@ export default {
 }
 const box = {
   A: {
-    mon: [30, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
-    tue: [30, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
+    mon: [99, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
+    tue: [1, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
     wed: [30, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
     thu: [30, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
     fri: [30, 58, 87, 42, 64, 29, 75, 38, 46, 96, 36, 68, 36, 58, 63, 58, 75, 67, 90, 66, 44, 66, 88, 63],
